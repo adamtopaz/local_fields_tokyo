77c64f90783e63cc234c46260c40e1fcdf5deb0e
adamtopaz <github@adamtopaz.com>
2025-07-01 23:51:59 -0600

    statement

--- LocalFieldsTokyo/Basic.lean
+++ LocalFieldsTokyo/Basic.lean
@@ -1,1 +1,18 @@
-def hello := "world"
+import Mathlib
+
+-- Let K be a complete valued field.
+variable (K : Type) [Field K] [ValuativeRel K]
+  [UniformSpace K] [IsTopologicalDivisionRing K]
+  [CompleteSpace K] [ValuativeTopology K]
+
+
+theorem main_theorem : List.TFAE [
+    LocallyCompactSpace K,
+    IsCompact ((ValuativeRel.valuation K).valuationSubring : Set K),
+    (ValuativeRel.IsDiscrete K) ∧
+    (ValuativeRel.IsRankLeOne K) ∧
+    (ValuativeRel.IsNontrivial K) ∧
+    Finite (IsLocalRing.ResidueField <|
+      ValuativeRel.valuation K |>.valuationSubring)
+    ] :=
+  sorry
